387fc223ea44c1b4137d0fd45456d8021153a4bd
Mohaqq <60523928+Mohaqq@users.noreply.github.com>
2021-06-28 22:29:03 +0300

    Add files via upload

---
+++ two_DCMotor_with_h_barge_l298.ino
@@ -0,0 +1,17 @@
+void setup()
+{
+ pinMode(8,OUTPUT);
+ pinMode(9,OUTPUT);
+ pinMode(10,OUTPUT);
+ pinMode(11,OUTPUT);
+ pinMode(12,OUTPUT);
+ pinMode(13,OUTPUT);
+}
+
+void loop()
+{
+digitalWrite(2,1);  
+digitalWrite(4,0);
+digitalWrite(7,1);
+digitalWrite(8,0);
+ delay(5000); }
